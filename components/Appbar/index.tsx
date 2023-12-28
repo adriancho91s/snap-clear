06e8bf99a5d6f911f97601be9ac0623cb0e5b1ed
Adrián Fernando Gaitán Londoño <72105546+adriancho91s@users.noreply.github.com>
2023-12-28 01:49:03 -0500

    fix mobile

--- components/Appbar/index.tsx
+++ components/Appbar/index.tsx
@@ -12,7 +12,7 @@ export default function App() {
   const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const isMobile = typeof window !== "undefined" && window.innerWidth < 900;
+  const isMobile = window.innerWidth < 1090;
 
   const { t: tAppBar } = useTranslation();
 
