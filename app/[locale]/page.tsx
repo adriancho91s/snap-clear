184396cafd05e27b95df34a2a1425d29c028d60b
Adrián Fernando Gaitán Londoño <72105546+adriancho91s@users.noreply.github.com>
2023-12-28 01:05:44 -0500

    Fix TS errors

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,12 +2,11 @@ import ExampleClientComponent from '@/components/ExampleClientComponent';
 import styles from './page.module.css';
 import Link from 'next/link';
 import initTranslations from '../i18n';
-import TranslationsProvider from '@/components/TranslationsProvider';
 
 const i18nNamespaces = ['home', "appBar"];
 
 async function Home({ params: { locale } }: { params: { locale: string } }) {
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+  const { t } = await initTranslations(locale, i18nNamespaces);
 
   return (
     // <TranslationsProvider
