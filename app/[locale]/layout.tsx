183293cc323b8ef14ae02415307f22a5d32f04d1
Adrián Gaitán <adrian.gaitan@utp.edu.co>
2023-12-28 19:42:37 -0500

    Fix TS errors

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
             locale={locale}
             resources={resources}
             >
-            <Appbar initialTheme={ ""} />
+            <Appbar />
             {children}
           </TranslationsProvider>
         </Providers>
